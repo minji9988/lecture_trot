0200d76294d75cefa83ae8e768f49295b7b522f7
minji9988 <rnwjf111@daum.net>
2023-03-28 21:50:20 +0900

    1

--- app/src/main/java/com/example/trot1/Singer1Fragment.kt
+++ app/src/main/java/com/example/trot1/Singer1Fragment.kt
@@ -7,11 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class Singer1Fragment : Fragment() {
 
@@ -24,13 +22,33 @@ class Singer1Fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         val view = inflater.inflate(R.layout.fragment_singer1, container, false)
+
+
+        val items = mutableListOf<String>()
+        items.add("1")
+        items.add("노래2")
+        items.add("노래3")
+
+        items.add("노래1")
+        items.add("노래2")
+        items.add("노래3")
 
 //        view.findViewById<ImageView>(R.id.image1).setOnClickListener {
 //
 //        }
 // 위에처럼 작성할 수도 있고, 아래 형태로 작성할 수도 있다.
+
+        //아래에 view는 fragment_singer1.xml을 뜻한다.
+        // 아래 rv 변수는 fragment_singer1.xml 안에 있는 singRV를 의미한다.
+        val rv = view.findViewById<RecyclerView>(R.id.singRV)
+        val rvAdapter = RVAdapter(items)
+
+
+        //rv 어뎁터를 연결해준다.
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(context)
 
 
         //2번 이미지 클릭했을 때 2번 fragment로 moving
